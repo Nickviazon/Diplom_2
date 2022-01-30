aa0fa7dedacb023d7c42ee65061a973697bdf601
Vitaliy Fundovoy <nickvi@bk.ru>
2022-01-30 12:19:41 +0300

    Test Profile register added

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,39 @@
+import clients.AuthClient;
+import clients.Profile.Profile;
+import clients.Profile.ProfileBuilder;
+import clients.Profile.ProfileDirector;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthTest {
+
+    private AuthClient authClient;
+
+    @Before
+    public void setUp() {
+        authClient = new AuthClient();
+    }
+
+    @Test
+    public void authRegisterReturns200WithAccessTokenForNewProfile() {
+
+        ProfileDirector profileDirector = new ProfileDirector();
+        ProfileBuilder profileBuilder = new ProfileBuilder();
+        profileDirector.buildFullProfile(profileBuilder);
+        Profile profile = profileBuilder.getResult();
+
+        Response registerResponse = authClient.registerProfileResponse(profile);
+        ValidatableResponse validatableResponse = registerResponse.then().assertThat().statusCode(200);
+        Boolean isResponseSuccessful = validatableResponse.extract().path("success");
+        String authToken = validatableResponse.extract().path("accessToken");
+        String refreshToken = validatableResponse.extract().path("refreshToken");
+
+        assertTrue(isResponseSuccessful);
+        assertNotNull(authToken);
+        assertNotNull(refreshToken);
+    }
+}
---
+++ src/test/java/clients/AuthClient.java
@@ -0,0 +1,23 @@
+package clients;
+
+import clients.Profile.Profile;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class AuthClient extends RestAssuredClient{
+
+    private static final String AUTH_PATH = "api/auth/";
+    private String accessToken;
+
+    public String getAccessToken(Profile profile) {
+        return accessToken;
+    }
+
+    public Response registerProfileResponse(Profile profile) {
+        RequestSpecification specification = getBaseSpec();
+        Method requestType = Method.POST;
+        String requestPath = AUTH_PATH + "register";
+        return getResponse(specification, requestType, requestPath, profile);
+    }
+}
